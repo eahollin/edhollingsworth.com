a573ab71bb7e772a86135a94e3208772ee74510d
Ed Hollingsworth <ed.hollingsworth@ionep.com>
2020-02-27 18:37:33 -0500

    Test db access

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'EdHollingsworth.com' });
+    res.render('index', { title: 'EdHollingsworth.com' });
+});
+
+/* GET dbtest page. */
+router.get('/dbtest', function(req, res) {
+    // Do database stuff here...
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/local', {useNewUrlParser: true});
+    var db = mongoose.connection;
+
+    var message = "Fail";
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        // we're connected!
+        message = "Success!";
+    });
+
+
+
+    res.render('dbtest', { title: 'Database Test', message: message});
 });
 
 module.exports = router;
