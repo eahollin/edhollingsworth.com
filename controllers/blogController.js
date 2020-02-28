e069122048fb5f381f8b299a90081ab9fc18bd60
Ed Hollingsworth <ed.hollingsworth@ionep.com>
2020-02-28 11:31:16 -0500

    Updated names to reflect blog instead of "dbtest"; moved & renamed location for db model file(s)

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,17 +1,14 @@
 // use db stuff already initialized
-var blog_posts = require('../db/schema/blogpost.js');
+var blog_posts = require('./model/blog_posts.js');
 
 // Display list of all posts
 exports.post_list = function(req, res, next) {
-    //res.send('NOT IMPLEMENTED: Post list');
-
     blog_posts.find({}, 'title body', function(err, posts) {
         if (err) return next(err);
-        // should have our posts...
+        // provide console feedback, then render the view...
         if (posts == null) console.log("returned array is null!");
         else console.log("found " + posts.length + " posts!");
 
-        res.render('dbtest', { title: 'Database Test', posts: posts});
+        res.render('blog', { title: 'Database Test', posts: posts});
     });
-
 };    
--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,9 @@ router.get('/', function(req, res) {
     res.render('index', { title: 'EdHollingsworth.com' });
 });
 
-/* GET dbtest page. */
-var post_controller = require('../controllers/postController');
+/* GET blog page. */
+var blog_controller = require('../controllers/blogController');
 
-router.get('/dbtest', post_controller.post_list); 
-
-/*function(req, res) {
-    // use db stuff already initialized
-    var mongoose = require('mongoose');
-    var BlogPost = require('../db/schema/blogpost.js');
-    
-    mongoose.connect('mongodb://dbuser:password@localhost:27017/edweb', {useNewUrlParser: true});
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    var post = BlogPost.findOne({}, 'title body', function(err, rec) {
-        if (err) return err.message;
-        // should have our posts...
-        console.log("found " + rec.title + "!");
-        return recs;
-    });
-    res.render('dbtest', { title: 'Database Test', posts: post.title});
-});*/
+router.get('/blog', blog_controller.post_list);
 
 module.exports = router;
