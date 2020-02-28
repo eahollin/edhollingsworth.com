69e00c7a391d35df5320f945071ee865db588d1d
Ed Hollingsworth <ed@edhollingsworth.com>
2020-02-28 22:25:13 +0000

    Further refinements to the layout and design of the blog template

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,12 +3,12 @@ var blog_posts = require('../model/blog_posts.js');
 
 // Display list of all posts
 exports.post_list = function(req, res, next) {
-    blog_posts.find({}, 'title body', function(err, posts) {
+    blog_posts.find({}, 'title body tags', function(err, posts) {
         if (err) return next(err);
         // provide console feedback, then render the view...
         if (posts == null) console.log("returned array is null!");
         else console.log("found " + posts.length + " posts!");
 
-        res.render('blog', { title: 'Database Test', posts: posts});
+        res.render('blog', { title: 'My Fantastic Blog', posts: posts});
     });
 };    
