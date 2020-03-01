88fd8bd7ce2c1437cda1b62d97cad64977a0c396
Ed Hollingsworth <ed.hollingsworth@ionep.com>
2020-02-29 19:13:34 -0500

    Attempt to fix Angular blog controller

--- public/core.js
+++ public/core.js
@@ -2,41 +2,46 @@
 
 var myBlog = angular.module("myBlog", []);
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+(function (app) {
+    app.controller("mainController", function mainController() {
+        var blog = this;
 
-    // when landing on the page, get all blog posts and show them
-    $http.get('/api/blog')
-        .success(function(data) {
-            $scope.posts = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        }) ;
-
-    // when submitting the add form, send the text to the node API
-    $scope.createPost = function() {
-        $http.post('/api/blog', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.posts = data;
+        // when landing on the page, get all blog posts and show them
+        $http.get('/api/blog')
+            .success(function (data) {
+                blog.posts = data;
                 console.log(data);
             })
-            .error(function(data) {
+            .error(function (data) {
                 console.log('Error: ' + data);
             });
-    };
 
-    // delete a post after checking it
-    $scope.deletePost = function(id) {
-        $http.delete('/api/blog/' + id)
-            .success(function(data) {
-                $scope.posts = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            })
-    };
-}
+
+        blog.formData = {};
+
+        // when submitting the add form, send the text to the node API
+        blog.createPost = function () {
+            $http.post('/api/blog', $scope.formData)
+                .success(function (data) {
+                    blog.formData = {}; // clear the form so our user is ready to enter another
+                    blog.posts = data;
+                    console.log(data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });
+        };
+
+        // delete a post after checking it
+        blog.deletePost = function (id) {
+            $http.delete('/api/blog/' + id)
+                .success(function (data) {
+                    blog.posts = data;
+                    console.log(data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                })
+        };
+    });
+})(myBlog);
